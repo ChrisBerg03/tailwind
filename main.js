c46dfdfa9bb17623c9b6039bd92efdaa31305d56
LAPTOP-4RIM9K97\Panda <chrisrberg03@gmail.com>
2024-11-01 10:42:57 +0100

    more tailwind

--- main.js
+++ main.js
@@ -19,13 +19,15 @@ async function displayData() {
     const container = document.querySelector("main");
     posts.forEach((item) => {
         container.innerHTML += `
-      <div class="w-96 bg-slate-300 border-gray-400 border-solid border">
-      <img src="${item.image}"/>
+      <div class=" bg-slate-300 border-gray-400 border-solid border">
+      <img src="${item.image}" alt="laptop image"/>
+      <div class="itemContainer duration-700 hover:bg-red-800">
         <h3>Name: ${item.title}</h3>
         <p>About: ${item.description}</p>
         <p>Specs: ${item.specs}</p>
         <p>Price: ${item.price}</p>
         <p>Stock: ${item.stock}</p>
+        </div>
       </div>
       `;
     });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./*.js"],
+    content: ["./*.js", "./*.html"],
     theme: {
         extend: {},
     },
